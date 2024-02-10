0caf64f292cc162ee53d975c864539c5dfb583a0
Lupnis J H <65355238+lupnis@users.noreply.github.com>
2024-02-10 13:26:19 +0800

    upload file fetcher class & declarations

---
+++ jmfnode/FileFetcher.cpp
@@ -0,0 +1,197 @@
+/*
+ * file name:       FileFetcher.cpp
+ * created at:      2024/01/29
+ * last modified:   2024/02/01
+ * author:          lupnis<lupnisj@gmail.com>
+ */
+
+#include "FileFetcher.h"
+
+namespace JRequests {
+FileFetcher::FileFetcher(QString proxy_host, quint16 proxy_port,
+                         QNetworkProxy::ProxyType proxy_type, quint32 timeout,
+                         quint32 max_retries, qint64 buffer_size) {
+    this->request.setProxy(proxy_host, proxy_port, proxy_type);
+    this->request.setBufferSize(buffer_size);
+    this->countdown = this->timeout = timeout;
+    this->max_retries = max_retries;
+    this->request_count = 0;
+    connect(&this->request, &RequestMeta::readyRead, this,
+            &FileFetcher::onFileWriteReady);
+}
+FileFetcher::~FileFetcher() { this->reset(); }
+
+Mode FileFetcher::getMode() const { return this->request.getMode(); }
+Status FileFetcher::getRequestStatus() const {
+    return this->request.getStatus();
+}
+Status FileFetcher::getFetcherStatus() const { return this->fetcher_status; }
+Result FileFetcher::getResult() const { return this->request.getResult(); }
+QPair<quint64, quint64> FileFetcher::getProgress() const {
+    return this->request.getProgress();
+}
+bool FileFetcher::getFinished() const {
+    return this->fetcher_status == Status::Finished;
+}
+bool FileFetcher::getFailed() const {
+    return this->fetcher_status == Status::Canceled ||
+           (this->fetcher_status == Status::Finished &&
+            this->request.getFailed());
+}
+qint64 FileFetcher::getBufferSize() const {
+    return this->request.getBufferSize();
+}
+quint32 FileFetcher::getTimeConsumed() const {
+    return this->timeout - this->countdown;
+}
+quint32 FileFetcher::getRequestCount() const { return this->request_count; }
+bool FileFetcher::getFileStoredAtMem() const { return this->store_in_memory; }
+QByteArray FileFetcher::getFileData() const { return this->file_data; }
+QString FileFetcher::getFileStoragePath() const { return this->file_path; }
+
+void FileFetcher::setProxy(QString proxy_host, quint16 proxy_port,
+                           QNetworkProxy::ProxyType proxy_type) {
+    this->request.setProxy(proxy_host, proxy_port, proxy_type);
+}
+void FileFetcher::setTimeout(quint32 timeout) { this->timeout = timeout; }
+void FileFetcher::setMaxRetries(quint32 max_retries) {
+    this->max_retries = max_retries;
+}
+void FileFetcher::setBufferSize(qint64 buffer_size) {
+    this->request.setBufferSize(buffer_size);
+}
+bool FileFetcher::setTask(QString url, QHash<QString, QString> headers,
+                          bool store_in_memory, QString file_path) {
+    if (this->lock.tryLock()) {
+        this->request.get(url, headers);
+        this->file_path = file_path;
+        if (file_path != "") {
+            this->store_in_memory = store_in_memory;
+        } else {
+            this->store_in_memory = true;
+        }
+        this->lock.unlock();
+        return true;
+    }
+    return false;
+}
+
+bool FileFetcher::run() {
+    if (this->lock.tryLock()) {
+        this->fetcher_status = Status::Fetching;
+        if (!this->store_in_memory) {
+            if (QFile(this->file_path).exists()) {
+                QFile(this->file_path).remove();
+            } else {
+                QString abs_path =
+                    QDir().absoluteFilePath(this->file_path).replace('\\', '/');
+                abs_path = abs_path.left(abs_path.lastIndexOf('/'));
+                if (!QDir(abs_path).exists()) {
+                    QDir().mkpath(abs_path);
+                }
+            }
+        }
+        this->tick_timer = new QTimer();
+        this->tick_timer->setInterval(1000);
+        connect(this->tick_timer, &QTimer::timeout, this,
+                &FileFetcher::onRequestTimeout);
+        this->tick_timer->start();
+        this->request.run();
+        return true;
+    }
+    return false;
+}
+
+void FileFetcher::abort() {
+    if (this->fetcher_status == Status::Fetching) {
+        this->fetcher_status = Status::Canceled;
+        this->request.abort();
+    }
+}
+
+void FileFetcher::reset() {
+    this->abort();
+    if (this->tick_timer != nullptr) {
+        this->tick_timer->stop();
+        this->tick_timer->deleteLater();
+        this->tick_timer = nullptr;
+    }
+    this->request.reset();
+    this->countdown = this->timeout;
+    this->request_count = 0;
+    this->file_data.clear();
+    this->file_path.clear();
+    this->fetcher_status = Status::Init;
+    this->lock.unlock();
+}
+
+void FileFetcher::onFileWriteReady() {
+    this->request.blockSignals(true);
+    QThread* file_append_thread = new QThread();
+    connect(file_append_thread, &QThread::finished, file_append_thread,
+            &QThread::deleteLater);
+    connect(file_append_thread, &QThread::started, [this] {
+        if (this->store_in_memory) {
+            while (this->request.hasNextPendingReply()) {
+                this->file_data += this->request.getReplyData();
+            }
+        } else {
+            QFile file(this->file_path);
+            if (file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+                while (this->request.hasNextPendingReply()) {
+                    file.write(this->request.getReplyData());
+                    file.flush();
+                }
+                file.close();
+            }
+        }
+        this->request.blockSignals(false);
+        QThread::currentThread()->quit();
+    });
+    file_append_thread->start();
+}
+
+void FileFetcher::onRequestTimeout() {
+    switch (this->request.getStatus()) {
+        case Status::Finished:
+            if (this->request.getFailed()) {
+                if (this->request.signalsBlocked()) {
+                } else {
+                    if (this->countdown &&
+                        this->request_count < this->max_retries) {
+                        this->request_count++;
+                        QPair<QString, QHash<QString, QString>> task =
+                            this->request.getTask();
+                        this->request.reset();
+                        if (this->store_in_memory) {
+                            this->file_data.clear();
+                        } else {
+                            if (QFile(this->file_path).exists()) {
+                                QFile(this->file_path).remove();
+                            }
+                        }
+                        this->request.get(task.first, task.second);
+                        this->request.run();
+                    }
+                }
+            } else {
+                this->tick_timer->stop();
+                this->fetcher_status = Status::Finished;
+                return;
+            }
+            break;
+        case Status::Canceled:
+            this->tick_timer->stop();
+            return;
+        default:
+            break;
+    }
+    if (!this->countdown) {
+        this->abort();
+        this->tick_timer->stop();
+    } else {
+        this->countdown--;
+    }
+}
+
+}  // namespace JRequests
---
+++ jmfnode/FileFetcher.h
@@ -0,0 +1,75 @@
+/*
+ * file name:       FileFetcher.h
+ * created at:      2024/01/23
+ * last modified:   2024/02/01
+ * author:          lupnis<lupnisj@gmail.com>
+ */
+
+#ifndef FILE_FETCHER_H
+#define FILE_FETCHER_H
+
+#include <QDir>
+#include <QFile>
+#include <QObject>
+#include <QThread>
+#include <QTimer>
+
+#include "RequestMeta.h"
+
+namespace JRequests {
+
+class FileFetcher : public QObject {
+    Q_OBJECT
+   public:
+    FileFetcher(QString proxy_host = "", quint16 proxy_port = 0,
+                QNetworkProxy::ProxyType proxy_type =
+                    QNetworkProxy::ProxyType::HttpProxy,
+                quint32 timeout = 60, quint32 max_retries = 8,
+                qint64 buffer_size = 1048576);
+
+    ~FileFetcher();
+
+    Mode getMode() const;
+    Status getRequestStatus() const;
+    Status getFetcherStatus() const;
+    Result getResult() const;
+    QPair<quint64, quint64> getProgress() const;
+    bool getFinished() const;
+    bool getFailed() const;
+    qint64 getBufferSize() const;
+    quint32 getTimeConsumed() const;
+    quint32 getRequestCount() const;
+    bool getFileStoredAtMem() const;
+    QByteArray getFileData() const;
+    QString getFileStoragePath() const;
+
+    void setProxy(QString proxy_host = "", quint16 proxy_port = 0,
+                  QNetworkProxy::ProxyType proxy_type =
+                      QNetworkProxy::ProxyType::HttpProxy);
+    void setTimeout(quint32 timeout = 60);
+    void setMaxRetries(quint32 max_retries = 8);
+    void setBufferSize(qint64 buffer_size = 1048576);
+
+    bool setTask(QString url, QHash<QString, QString> headers = BASE_HEADERS,
+                 bool store_in_memory = true, QString file_path = "");
+    bool run();
+    void abort();
+    void reset();
+
+   private slots:
+    void onFileWriteReady();
+    void onRequestTimeout();
+
+   private:
+    RequestMeta request;
+    quint32 timeout, countdown, max_retries, request_count;
+    bool store_in_memory;
+    QString file_path;
+    QByteArray file_data;
+    QTimer* tick_timer = nullptr;
+    Status fetcher_status = Status::Init;
+    QMutex lock;
+};
+}  // namespace JRequests
+
+#endif
