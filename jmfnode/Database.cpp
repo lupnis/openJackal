b2e4553786173bd59959381c43a6ac4a8804560b
petit loup <65355238+lupnis@users.noreply.github.com>
2024-02-21 23:31:14 +0800

    Update Database.cpp

--- jmfnode/Database.cpp
+++ jmfnode/Database.cpp
@@ -193,7 +193,7 @@ QPair<int, QList<QList<QVariant>>> MySQLODBCController::upsert(
         if (i) {
             sql_cmd += ", ";
         }
-        sql_cmd += QString("%1").arg(record[i].toString().replace("\'", "\\'"));
+        sql_cmd += QString("%1").arg(record[i].toString());
     }
     sql_cmd += ") on duplicate key update ";
     for (int i = 0; i < record_on_duplicate.size(); ++i) {
@@ -203,8 +203,9 @@ QPair<int, QList<QList<QVariant>>> MySQLODBCController::upsert(
         sql_cmd +=
             QString("`%1`=%2")
                 .arg(columns[i])
-                .arg(record_on_duplicate[i].toString().replace("\'", "\\'"));
+                .arg(record_on_duplicate[i].toString());
     }
+    qDebug() << sql_cmd;
     return this->runsql(sql_cmd);
 }
 
