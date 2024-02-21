8ed3317a1560b00dff798f046b6836381b16c35c
Lupnis J H <Lupnis@outlook.com>
2024-02-22 03:11:09 +0800

    update main.cpp

--- src/jmfnode/main.cpp
+++ src/jmfnode/main.cpp
@@ -12,10 +12,14 @@
 int main(int argc, char* argv[]) {
     QCoreApplication a(argc, argv);
     JConfigs::JsonConfigHandler config_handler;
-    if(argc > 1 && QFile(argv[1]).exists()) {
+    if (argc > 1 && QFile(argv[1]).exists()) {
         config_handler.setFilePath(argv[1]);
+    } else if (QFile().exists("node_properties.json")) {
+        config_handler.setFilePath("node_properties.json");
     } else {
-        std::cout << "no config file found, load default config file: node_properties.json..." << std::endl;
+        std::cout << "no config file found, create and load default config file: "
+                     "node_properties.json..."
+                  << std::endl;
     }
     JackalMFN::NodeController controller(config_handler);
     controller.startNode();
